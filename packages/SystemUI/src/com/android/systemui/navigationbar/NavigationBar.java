e970acdff6a2965542a98da875afe5377e3108c5
bross1_kuo <bross1_kuo@asus.com>
2023-06-19 18:02:39 +0800

    display: keep display rotation settings after reboot
    if config_supportAutoRotation is set to false,
    
    rotation setting will be set to 0 degree,
    
    also set config_enableLockScreenRotation to true to rotate lockscreen
    
    Change-Id: I9678f16587f65c21ac230e6734679b67d2955d3c
    Signed-off-by: bross1_kuo <bross1_kuo@asus.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -899,7 +899,7 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
             // Reset user rotation pref to match that of the WindowManager if starting in locked
             // mode. This will automatically happen when switching from auto-rotate to locked mode.
             if (display != null && rotationButtonController.isRotationLocked()) {
-                rotationButtonController.setRotationLockedAtAngle(display.getRotation());
+                //rotationButtonController.setRotationLockedAtAngle(display.getRotation());
             }
         } else {
             mDisabledFlags2 |= StatusBarManager.DISABLE2_ROTATE_SUGGESTIONS;
