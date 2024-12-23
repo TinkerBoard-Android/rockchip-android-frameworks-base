0aa82ff891dfc628900327ee2622ab9a81837da8
Tzuhsuan_Chen <Tzuhsuan_Chen@asus.com>
2024-12-23 14:09:50 +0800

    RVMON7: Change the launcher to normal without go.
    Change-Id: Ieced89b1aa5d3f95b1dbb0e752ff161225b0c32a

--- core/java/com/android/internal/app/ResolverActivity.java
+++ core/java/com/android/internal/app/ResolverActivity.java
@@ -376,10 +376,7 @@ public class ResolverActivity extends Activity implements
         String homeAcitvityName = "";
         String homePackageName = SystemProperties.get("persist.home.package", "com.android.launcher3");
         Log.d(TAG, "Build.MODEL = " + Build.MODEL);
-        if (Build.MODEL.equals("RVMON7 CTRL PCB"))
-            homeAcitvityName = SystemProperties.get("persist.home.activity", "com.android.launcher3.Launcher3QuickStepGo");
-        else
-            homeAcitvityName = SystemProperties.get("persist.home.activity", "com.android.launcher3.uioverrides.QuickstepLauncher");
+        homeAcitvityName = SystemProperties.get("persist.home.activity", "com.android.launcher3.uioverrides.QuickstepLauncher");
 
         if (mResolvingHome) {
             setDefaultLauncher(homePackageName, homeAcitvityName);
