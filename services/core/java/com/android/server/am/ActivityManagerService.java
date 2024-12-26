8b5df62684bafd4abd6789074532a8da3ae45703
Tzuhsuan Chen <Tzuhsuan_Chen@asus.com>
2024-12-26 09:22:47 +0800

    Revert "AMS: reduce the delay time to launch app on emmc for RVMON7 CTRL PCB"
    This reverts commit c2c786f9d797d13312ccefd51e1cc9c0b8400a79.
    
    Change-Id: I7cbd29c5292116b068520b0c89d66da5b9900bb5

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -7815,13 +7815,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         PackageManager doupackageManager = context.getPackageManager();
         Log.d(TAG, "startAppOrService");
         int delayTime;
-
-        if ("emmc".equals(SystemProperties.get("ro.boot.storagemedia"))) {
-            Log.d(TAG, "storagemedia is emmc, delay time is 5s");
-            delayTime = 5000;
-        } else {
-            delayTime = 8000;
-        }
+        delayTime = 8000;
         try {
             new Handler().postDelayed(new Runnable() {
                 public void run() {
